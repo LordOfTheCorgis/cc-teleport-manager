ea5a38d77babd5282f33613b77747142318a7222
LordOfTheCorgis <128561696+LordOfTheCorgis@users.noreply.github.com>
2025-12-25 12:59:12 -0600

    Add dependencies to fxmanifest.lua
    Added dependencies for ox_lib and ox_target.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,11 @@ author 'LordOfTheCorgis'
 description 'Very simple marker-based teleport script.'
 version '1.0.0'
 
+dependencies {
+    'ox_lib',
+    'ox_target'
+}
+
 shared_script {
     'config.lua',
     '@ox_lib/init.lua'
